fcb81ebbfdde595672582eda86fef900aeff3700
brocode <kelimbth@gmail.com>
2023-11-17 15:45:48 +0300

    first commit 🚀

---
+++ script.r
@@ -0,0 +1,47 @@
+# This is my first R script.
+
+# Date: 2023-11-17 1400HRS
+# Understanding R basics and performing calculations with R
+
+addition <- 1 + 2
+addition
+
+# subtraction
+subtraction <- 4 - 3
+subtraction
+
+# division
+division <- 189 / 67
+division
+
+# modulus
+modulus <- 40 %% 6
+modulus
+
+# square root
+square_root <- sqrt(25)
+square_root
+
+# Standard Deviation
+x <- c(12, 10, 40, 15, 25)
+standard_dev <- sd(x)
+standard_dev
+
+# variance
+variance <- var(x)
+variance
+
+square_root_negative <- sqrt(-25)
+square_root_negative <- sqrt(-25 + 0i)
+square_root_negative
+
+colon <- (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+colon
+colon_func <- c(1:10)
+colon_func
+
+sequency <- seq(50, 100, by=5)
+sequency
+
+_sequency <- seq(1, 10, length=10)
+_sequency
